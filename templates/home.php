7e6bb5605c572a946c5f4434c16e86bcb1a7f61a
kantum21 <quentinsporn@gmail.com>
2019-09-25 17:31:50 +0200

    SetUp MVC

--- templates/home.php
+++ templates/home.php
@@ -1,34 +1,18 @@
-<?php
-use App\src\DAO\ArticleDAO;
-?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <title>Mon blog</title>
-</head>
+<?php $this->title = "Accueil"; ?>
 
-<body>
-<div>
     <h1>Mon blog</h1>
     <p>En construction</p>
-    <?php
-    $article = new ArticleDAO();
-    $articles = $article->getArticles();
-    while($article = $articles->fetch())
-    {
-        ?>
-        <div>
-            <h2><a href="../public/index.php?route=article&articleId=<?= htmlspecialchars($article->id);?>"><?= htmlspecialchars($article->title);?></a></h2>
-            <p><?= htmlspecialchars($article->content);?></p>
-            <p><?= htmlspecialchars($article->author);?></p>
-            <p>Créé le : <?= htmlspecialchars($article->createdAt);?></p>
-        </div>
-        <br>
-        <?php
-    }
-    $articles->closeCursor();
+<?php
+foreach ($articles as $article)
+{
     ?>
-</div>
-</body>
-</html>
+    <div>
+        <h2><a href="../public/index.php?route=article&articleId=<?= htmlspecialchars($article->getId());?>"><?= htmlspecialchars($article->getTitle());?></a></h2>
+        <p><?= htmlspecialchars($article->getContent());?></p>
+        <p><?= htmlspecialchars($article->getAuthor());?></p>
+        <p>Créé le : <?= htmlspecialchars($article->getCreatedAt());?></p>
+    </div>
+    <br>
+    <?php
+}
+?>
